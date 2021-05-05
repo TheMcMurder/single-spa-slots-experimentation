f24a38e215b8237992c1d36f8c060c4c56ca5c82
Justin McMurdie <mcmurdiej@gmail.com>
2021-05-05 10:42:30 -0600

    single-spa-layout overrides

--- packages/root-config/src/root-config.js
+++ packages/root-config/src/root-config.js
@@ -6,20 +6,93 @@ import {
   constructLayoutEngine,
 } from 'single-spa-layout';
 
-const routes = constructRoutes(document.querySelector('#single-spa-layout'));
+const tasksActivityFn = (location) => location.pathname.startsWith('/tasks') || location.pathname.includes('/tasks/')
+const usersActivityFn = (location) => location.pathname.startsWith('/users') && !location.pathname.includes('tk-421')
+
+// const routesFromHTML = constructRoutes(document.querySelector('#single-spa-layout'))
+// window.routes = routesFromHTML
+const tasksApplication = {
+  type: 'application',
+  name: 'tasks',
+}
+const usersApplication = {
+  type: 'application',
+  name: 'users',
+}
+
+const routes = constructRoutes({
+  mode: 'history',
+  base: '/',
+  routes: [
+    {
+      type: 'nav',
+      attrs: [{name: 'class', value: 'main-nav'}],
+      routes: [
+        {
+          type: 'application',
+          name: 'primary-nav',
+        },
+      ]
+    },
+    {
+      type: 'main',
+      attrs: [{name: 'class', value: 'main-content'}],
+      routes: [
+        {
+          type: "route",
+          path: "tasks",
+          routes: [
+            tasksApplication
+          ],
+        },
+        {
+          type: "route",
+          path: "users",
+          routes: [
+            usersApplication,
+            {
+              type: 'route',
+              path: '/tasks/:id',
+              routes: [
+                tasksApplication
+              ]
+            },
+          ],
+        },
+      ]
+    }
+  ],
+  containerEl: 'body'
+})
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
     return System.import(name);
   },
+  activeWhenOverrides: {
+    'tasks': tasksActivityFn,
+    'users': usersActivityFn
+  }
 });
-console.log('applications', applications)
 const layoutEngine = constructLayoutEngine({ routes, applications });
 
-applications.forEach((app) => {
-  console.log('application', app)
-  registerApplication(app)
-});
+applications.forEach((app) => registerApplication(app))
+
+// applications.forEach((app) => {
+//   console.log('application', app)
+//   const existingActiveWhenFn = app.activeWhen[0]
+//   if(app.name === 'users') {
+//     registerApplication({
+//       ...app,
+//       activeWhen: (location) => {
+//         const isTK421 = location.pathname.includes('tk-421')
+//         return existingActiveWhenFn && !isTK421
+//       }
+//     })
+//   } else {
+//     registerApplication(app)
+//   }
+// });
 // import { fromEvent } from 'rxjs'
 // import { tap, shareReplay } from 'rxjs/operators'
 
