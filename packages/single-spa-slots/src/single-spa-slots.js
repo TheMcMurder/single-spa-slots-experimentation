486a0a2c43a3642f5d7bfe8b8d20f4b86ef76c10
the_mcmurder <the_mcmurder@users.noreply.github.com>
2021-05-18 15:13:42 -0600

    removing some apis

--- packages/single-spa-slots/src/single-spa-slots.js
+++ packages/single-spa-slots/src/single-spa-slots.js
@@ -1,4 +1,3 @@
-import { mountRootParcel } from 'single-spa'
 import { generateError } from './error-generate.js'
 
 const slots = {}
@@ -21,36 +20,4 @@ export function createSlots(config = {}) {
     }
     slots[key] = value
   })
-}
-
-// export function createSlot(name, domNode) {
-//     slots[name] = domNode
-//     return () => removeSlot(name)
-// }
-
-// export function removeSlot(name) {
-//     delete slots[name]
-// }
-
-export function fillSlot(name, parcelConfig, props = {}) {
-  if (!slots[name]) {
-    const error = generateError(`Slot by name of ${name} doesn't exist`)
-    throw error
-  }
-  const domElement = document.querySelector(slots[name])
-  if (!domElement) {
-    const error = generateError(
-      `cannot find dom node with querySelector: ${slots[name]}`,
-    )
-    throw error
-  }
-  const parcelProps = Object.assign(
-    {},
-    {
-      domElement,
-    },
-    props,
-  )
-  // todo use mouteParcel instead
-  return mountRootParcel(parcelConfig, parcelProps)
 }
