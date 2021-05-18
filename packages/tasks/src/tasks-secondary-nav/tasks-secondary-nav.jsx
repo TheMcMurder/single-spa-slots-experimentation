1d4b7bcf652443fd7ca315da41cfeede97d936cc
the_mcmurder <the_mcmurder@users.noreply.github.com>
2021-05-18 15:21:03 -0600

    more comments

--- packages/tasks/src/tasks-secondary-nav/tasks-secondary-nav.jsx
+++ packages/tasks/src/tasks-secondary-nav/tasks-secondary-nav.jsx
@@ -4,6 +4,8 @@ import { tw } from 'twind'
 import { useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import SearchIcon from './search-icon.jsx'
+
+// This no longer renders a parcel but it uses the <Slot> react component to render into a portal.
 export default function TasksSecondaryNavWrapper(props) {
   const location = useLocation()
   const { pathname } = location
